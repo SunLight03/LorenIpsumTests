b38d6b7d489250e524a8af796e98c13762c0f940
SunLight03 <90985382+SunLight03@users.noreply.github.com>
2021-10-27 18:25:14 +0300

    Update CheckTheProbability40Test.java

--- src/test/java/CheckTheProbability40Test.java
+++ src/test/java/CheckTheProbability40Test.java
@@ -3,13 +3,8 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class CheckTheProbability40Test extends BaseTest {
-    @Test
+   @Test
     public void checkTheProbability40() {
-        double all = 0;
-        for (int i = 0; i < 10; i++) {
-            getBLL().pressGenerateLorenIpsumButton();
-            all = getBLL().getNumberOfLorem();
-        }
-        assertTrue(all % 10.0 >= 2);
+        assertTrue(getBLL().getAverageOfTenGenerations() >= 2);
     }
 }
